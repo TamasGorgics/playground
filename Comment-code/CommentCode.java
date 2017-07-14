783ff6101327837b4345d58b72cfdc822d98bc37
Tamás Görgics <toma2067@gmail.com>
2017-07-15 00:01:39 +0200

    code in comment - java

---
+++ Comment-code/CommentCode.java
@@ -0,0 +1,10 @@
+package interesting;
+
+public class CommentCode {
+
+	public static void main(String... args) {
+		
+		// The next comment is not a typo
+		// \u000d System.out.println("Hello World!");
+	}
+}
